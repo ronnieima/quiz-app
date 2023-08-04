47ebe6469e17c26e598b8f80b62b02099e564601
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-05 00:05:45 +0200

    hide left arrow on first question

--- src/components/PrevButton.js
+++ src/components/PrevButton.js
@@ -2,7 +2,7 @@ function PrevButton({ dispatch, index }) {
   return (
     <button
       disabled={index === 0}
-      className="btn btn-navigate"
+      className={`btn btn-navigate ${index === 0 ? "hidden" : ""}`}
       onClick={() => dispatch({ type: "navigateLeft", payload: index })}
     >
       &larr;
